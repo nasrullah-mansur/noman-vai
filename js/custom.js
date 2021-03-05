43d172564b8d02e7fa18e2c73734bc46b77098ba
nasrullahmansur <nasrullahbd64@gmail.com>
2021-03-05 22:00:34 +0600

    home page

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    "use strict";
+
+    $('.banner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear'
+      });
+
+
+      $('.venobox').venobox(); 
+
+
+
+
+
+
+
+});
